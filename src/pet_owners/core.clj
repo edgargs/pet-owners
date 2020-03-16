89d423bc4f02f3e866e8c3a203528b45798a2250
Edgar Rios Navarro <edgar.gs@gmail.com>
2020-03-16 09:16:38 -0500

    Add test for query by owner name

---
+++ src/pet_owners/core.clj
@@ -0,0 +1,13 @@
+(ns pet-owners.core
+  (:require [datomic.api :as d]))
+
+(def conn nil)
+
+(defn add-pet-owner [owner-name]
+  @(d/transact conn [{:db/id (d/tempid :db.part/user)
+                      :owner/name owner-name}]))
+
+(defn find-all-pet-owners []
+  (d/q '[:find ?owner-name
+         :where [_ :owner/name ?owner-name]]
+        (d/db conn)))
