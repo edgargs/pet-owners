989242af8caa6b59067a4a50232137859c2cab77
Edgar Rios Navarro <edgar.gs@gmail.com>
2020-03-16 16:19:35 -0500

    Add test for query pets for owner

--- src/pet_owners/core.clj
+++ src/pet_owners/core.clj
@@ -11,3 +11,26 @@
   (d/q '[:find ?owner-name
          :where [_ :owner/name ?owner-name]]
         (d/db conn)))
+
+(defn find-pet-owner-id [owner-name]
+  (ffirst (d/q  '[:find ?eid
+                  :in $ ?owner-name
+                  :where [?eid :owner/name ?owner-name]]
+                (d/db conn)
+                owner-name)))
+
+(defn add-pet [pet-name owner-name]
+  (let [pet-id (d/tempid :db.part/user)]
+    @(d/transact conn [{:db/id pet-id
+                        :pet/name pet-name}
+                       {:db/id (find-pet-owner-id owner-name)
+                        :owner/pets pet-id}])))
+
+(defn find-pets-for-owner [owner-name]
+  (d/q '[:find ?pet-name
+         :in $ ?owner-name
+         :where [?eid :owner/name ?owner-name]
+                [?eid :owner/pets ?pet]
+                [?pet :pet/name ?pet-name]]
+       (d/db conn)
+       owner-name))
--- test/pet_owners/core_test.clj
+++ test/pet_owners/core_test.clj
@@ -12,8 +12,35 @@
       (d/transact conn schema)
       conn)))
 
+;; Adding one owner should allow us to find that owner
 (expect #{["Jhon"]}
   (with-redefs [conn (create-empty-in-memory-db)]
     (do 
       (add-pet-owner "Jhon")
       (find-all-pet-owners))))
+
+;; Adding multiple owners should allow us to find those owners
+(expect #{["Jhon"] ["Paul"] ["George"]}
+  (with-redefs [conn (create-empty-in-memory-db)]
+    (do 
+      (add-pet-owner "Jhon")
+      (add-pet-owner "Paul")
+      (add-pet-owner "George")
+      (find-all-pet-owners))))
+
+;; Adding one owner with one pet
+(expect #{["Salt"]}
+  (with-redefs [conn (create-empty-in-memory-db)]
+    (do
+      (add-pet-owner "Jhon")
+      (add-pet "Salt" "Jhon")
+      (find-pets-for-owner "Jhon"))))
+
+;; Adding one owner with multiple pets
+(expect #{["Pepper"] ["Jet"]}
+  (with-redefs [conn (create-empty-in-memory-db)]
+    (do
+      (add-pet-owner "Paul")
+      (add-pet "Pepper" "Paul")
+      (add-pet "Jet" "Paul")
+      (find-pets-for-owner "Paul"))))      
